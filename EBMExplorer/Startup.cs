3b3842f6695f79414c982fed9914c19c417d10df
zhangy <zhangy147@hotmail.com>
2016-06-26 23:25:16 -0400

    test

---
+++ EBMExplorer/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(EBMExplorer.Startup))]
+namespace EBMExplorer
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
